f38f8c084a0ed9bc29366d9a55c61a4c77728b2a
NAZEER HUSSAIN FAKEER MOHAMED <43745616+nazeerhussainf@users.noreply.github.com>
2023-05-23 20:24:44 +0530

    Update updateGlobalEnvVariable.groovy

--- vars/updateGlobalEnvVariable.groovy
+++ vars/updateGlobalEnvVariable.groovy
@@ -1,6 +1,6 @@
 def call(globVarName, globVarValue) {
   node {
-       instance = Jenkins.getInstance()
+       instance = Jenkins.get()
        def globalNodeProperties = instance.getGlobalNodeProperties()
        def envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
        if (envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0) {
