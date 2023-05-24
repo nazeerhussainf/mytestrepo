eb5598395af02f129e457de5f91f0f7cfafae4d7
NAZEER HUSSAIN FAKEER MOHAMED <43745616+nazeerhussainf@users.noreply.github.com>
2023-05-24 13:34:15 +0530

    Update updateGlobalEnvVariable.groovy

--- vars/updateGlobalEnvVariable.groovy
+++ vars/updateGlobalEnvVariable.groovy
@@ -8,11 +8,11 @@ def call(String globVarName, globVarValue) {
        def envVars = null
 
        if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
-           newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
-           globalNodeProperties.add(newEnvVarsNodeProperty)
-           envVars = newEnvVarsNodeProperty.getEnvVars()
+           def newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+           def globalNodeProperties.add(newEnvVarsNodeProperty)
+           def envVars = newEnvVarsNodeProperty.getEnvVars()
        } else {
-           envVars = envVarsNodePropertyList.get(0).getEnvVars()
+           def envVars = envVarsNodePropertyList.get(0).getEnvVars()
        }
 
        envVars.put(globVarName, globVarValue)
