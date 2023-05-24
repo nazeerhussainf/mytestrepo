4e46e271277e7e08e18cc98ffd8733030ef02e45
NAZEER HUSSAIN FAKEER MOHAMED <43745616+nazeerhussainf@users.noreply.github.com>
2023-05-24 19:51:57 +0530

    Update updateGlobalEnvVariable.groovy

--- vars/updateGlobalEnvVariable.groovy
+++ vars/updateGlobalEnvVariable.groovy
@@ -1,22 +1,20 @@
 @NonCPS
-def call(String globVarName, globVarValue) {
-       instance = Jenkins.get()
-       def globalNodeProperties = null
-       globalNodeProperties = instance.getGlobalNodeProperties()
-       def envVarsNodePropertyList = null
-       envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+def updateGlobalEnvVariable(String globVarName, globVarValue) {
+        def instance = null
+        instance = Jenkins.get()
+        def globalNodeProperties = instance.getGlobalNodeProperties()
+        def envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
 
-       def newEnvVarsNodeProperty = null
-       def envVars = null
+        def newEnvVarsNodeProperty = null
+        def envVars = null
 
-       if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
-           newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
-           globalNodeProperties.add(newEnvVarsNodeProperty)
-           envVars = newEnvVarsNodeProperty.getEnvVars()
-       } else {
-           envVars = envVarsNodePropertyList.get(0).getEnvVars()
-       }
+        if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+            newEnvVarsNodeProperty = new hudson.slaves.EnvironmentVariablesNodeProperty();
+            globalNodeProperties.add(newEnvVarsNodeProperty)
+            envVars = newEnvVarsNodeProperty.getEnvVars()
+        } else {
+            envVars = envVarsNodePropertyList.get(0).getEnvVars()
+        }
 
-       envVars.put(globVarName, globVarValue)
-       instance.save()
+        def updateStat = envVars.put(globVarName, globVarValue)
 }
